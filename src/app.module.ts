a5a9d636ece7d2c0a247fecbe2e5a6aa0d2dfc93
GaetanDuron <gaetan.duron@gmail.com>
2022-09-16 19:26:03 +0200

    use .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
+import Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,17 +11,22 @@ import { UserModule } from './users/users.module';
   imports: [
     ObservationTypeModule,
     UserModule,
+    ConfigModule.forRoot({validationSchema: Joi.object({
+      DATABASE_HOST: Joi.required(),
+      DATABASE_PORT: Joi.number().default(5432),
+    })
+  }),
     TypeOrmModule.forRoot(
       {
-        type: 'postgres', // type of our database
-        host: 'localhost', // database host
-        port: 5555, // database host
-        username: 'postgres', // username
-        password: 'pass123', // user password
-        database: 'postgres', // name of our database,
-        autoLoadEntities: true, // models will be loaded automatically 
-        synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
-      }
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true,
+      },
     )
   ],
   controllers: [AppController],
